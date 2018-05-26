c0a470fa120d7409cd1a30ab602b1ca90b4bf4c3
Ranats <sopurani885@gmail.com>
2018-05-26 23:10:30 +0900

    add an Assumed solution to B

--- ABC098/B.rb
+++ ABC098/B.rb
@@ -1,8 +1,29 @@
 n = gets.to_i
 s = gets.chomp.split("")
+=begin
+# 想定解?
+max = 0
+1.upto(n) do |i|
+  hash = {}
+  s[0...i].each do |si|
+    if hash[si].nil?
+      hash[si] = 1
+    end
+  end
+
+  count = 0
+  s[i..-1].each do |si|
+    count += 1 unless hash[si].nil?
+  end
+
+  max = [count,max].max
+end
+puts max
+=end
 
 max = 0
 1.upto(n) do |i|
+
   first = s[0...i].uniq.sort
   last = s[i..-1].uniq.sort
   num = first.size - (first - last).size
