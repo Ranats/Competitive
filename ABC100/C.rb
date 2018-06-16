7ee971b0af459c8bddc8251bf8c9807d004c4b65
Ranats <sopurani885@gmail.com>
2018-06-16 23:41:24 +0900

    AC to C

--- ABC100/C.rb
+++ ABC100/C.rb
@@ -1,39 +1,12 @@
-require 'prime'
+# どれかを必ず2で割らなければならない
+# => 何回2で割れるか，の合計
+# 3を何回掛けても，2で割れる回数は変わらない
+
+def division2(x,i)
+  x % 2 != 0 ? i : division2(x/2,i+1)
+end
 
 n = gets.to_i
 seq = gets.chomp.split(" ").map(&:to_i)
 
-seq.delete_if{|a| a%2 != 0}.sort!
-
-if seq.empty?
-  puts 0
-  exit
-end
-
-#p seq
-
-count = 0
-until seq.all?{|a| a%2 != 0}
-  count += seq.size
-  max = seq.select{|a| a % 2 == 0}.max
-
-  size = max.prime_division.size
-
-    seq.map! do |a|
-      a * (3 ** (seq.size-1)) / 2
-    end
-
-#  seq.map! do |a|
-#    if a == max
-#      a /= 2
-#    else
-#      a *= 3
-#    end
-#  end
-  
- p seq
-  seq.delete_if{|a| a%2 != 0}
-#  gets
-end
-
-p count
+p seq.inject(0){|sum,a| sum + division2(a,0)}
