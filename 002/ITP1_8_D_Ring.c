a81bd40efe525ad5888b5d28080c3e68777a6b94
Ranats <sopurani885@gmail.com>
2019-07-02 11:53:49 +0900

    Add files via upload

---
+++ 002/ITP1_8_D_Ring.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define TRUE 1
+#define FALSE 0
+
+typedef struct {
+	char val[100];
+	int length;
+}string;
+
+int get_length(const char *str){
+	int length = 0;
+	while (*str++){
+		length++;
+	}
+	return length;
+}
+
+int solve(string s, string p){
+	int i, j;
+
+	for (i = 0; i < s.length + p.length; i++){
+		for (j = 0; j < p.length; j++){
+			if (s.val[(i + j)%s.length] != p.val[j]){
+				break;
+			}
+			if (j == p.length - 1){
+				return TRUE;
+			}
+		}
+	}
+	return FALSE;
+}
+
+int main(){
+	string s, p;
+
+	scanf("%s", &s.val);
+	scanf("%s", &p.val);
+
+	s.length = get_length(s.val);
+	p.length = get_length(p.val);
+
+	(solve(s, p) == 1) ? puts("Yes") : puts("No");
+}
